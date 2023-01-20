14c7197bdbbf8d5b78b276aa0903d5e339aebfe5
Samarth Gupta <samarthgupta1202@gmail.com>
2023-01-20 12:03:05 +0530

    updated with useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 const Person = (props) => {
   return (
@@ -12,13 +13,18 @@ const Person = (props) => {
 }
 
 const App = () => {
-
+  const [counter, setCounter] = useState(0)
   return (
     <div className="App">
 
-      <Person name={"Samarth"} last={"Gupta"} age={20} />
+      {/* <Person name={"Samarth"} last={"Gupta"} age={20} />
       <Person name={"Vanshi"} last={"Shah"} age={19} />
-      <Person name={"Shawty"} last={"Tiwarekar"} age={19} />
+      <Person name={"Shawty"} last={"Tiwarekar"} age={19} /> */}
+
+      <button onClick={() => { setCounter((prevcount) => prevcount - 1) }}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => { setCounter((prevcount) => prevcount + 1) }}>+</button>
+
 
     </div>
   );
